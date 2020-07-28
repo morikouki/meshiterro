d210e20e141b253da99469d865039fe180cfd0f6
vagrant <akzouksia@icloud.com>
2020-07-28 02:32:41 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ee1162e2e4093d21a7d57fbcc9f5a1c3743da859867fa79ce8c3236fae136715da7fe263a35c648a6339878848b37a2f714859c458495709fe58288b28df43a9'
